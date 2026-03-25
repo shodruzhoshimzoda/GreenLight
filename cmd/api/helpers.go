819ff59342908ec9dce163cba1f8f82f1744e329
sabrrise <shodruzxoshimzoda@gmail.com>
2026-03-25 22:10:01 +0500

    Создание вспомогательных функций

---
+++ cmd/api/helpers.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+// readIDParams - данная вспомогательная функция используется для извлечения ID из тело URL и возвршает его
+func (app *application) readIDParams(r *http.Request) (int64, error) {
+	params := httprouter.ParamsFromContext(r.Context())
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+
+	if err != nil || id < 1 {
+		http.NotFound(nil, r)
+		return 0, errors.New("invalid id parameter")
+	}
+
+	return id, nil
+}
