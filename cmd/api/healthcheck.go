bbf9a0a5c011e239af9f7a6daf91d7ff0e2a48d3
sabrrise <shodruzxoshimzoda@gmail.com>
2026-03-27 20:22:55 +0500

    Отправка простогого JSON

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -6,8 +6,17 @@ import (
 )
 
 func (app *application) healthcheck(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintln(w, "environment: ", app.config.env)
-	fmt.Fprintln(w, "version: "+version)
+	//fmt.Fprintln(w, "status: available")
+	//fmt.Fprintln(w, "environment: ", app.config.env)
+	//fmt.Fprintln(w, "version: "+version)
+
+	json := `{"status": "available", "environment":%q, "version":%q}`
+
+	jsonTag := fmt.Sprintf(json, app.config.env, version)
+
+	// Change header
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write([]byte(jsonTag))
 
 }
